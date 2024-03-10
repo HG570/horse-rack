dd58c8d472e658c128e9d8655f434d8286b2d888
HG570 <rafaelpinheirovgp@gmail.com>
2024-03-10 15:57:59 -0300

    feat: routes vacancies

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar"
 import Home from './pages/Home'
 import Notifications from './pages/Notifications'
 import Account from './pages/Account'
+import Vacancies from './pages/Vacancies'
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/notificacoes" element={<Notifications />} />
           <Route path="/conta" element={<Account />} />
+
+          <Route path="/vagas" element={<Vacancies />} />
         </Routes>
         <Navbar />
       </Router>
