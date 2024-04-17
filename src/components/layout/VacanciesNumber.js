68db32787356664ff03eeb6cae34defc51586d29
HG570 <rafaelpinheirovgp@gmail.com>
2024-04-16 21:34:49 -0300

    build: structure, style and color logic of the number of vacancies

---
+++ src/components/layout/VacanciesNumber.js
@@ -0,0 +1,30 @@
+import styles from './VacanciesNumber.module.css'
+let number = 70;
+let occupation = styles.normal;
+
+const maxNumber = 200;
+const percentage = (number / maxNumber) * 100;
+
+
+function VacancieNumber(){
+    if (percentage >= 75) {
+        occupation = styles.high;
+    } else if (percentage > 25 && percentage < 75) {
+        occupation = styles.medium;
+    } else if (percentage <= 25) {
+        occupation = styles.low;
+    } else {
+        occupation = styles.normal;
+    }
+
+    return (
+        <>
+            <ul className={styles.vacancies_number}>
+                <li className={`${styles.number} ${occupation} `}>{number}</li>
+                <li className={styles.vacancy}>Vagas</li>
+            </ul>
+        </>
+    )
+}
+
+export default VacancieNumber
