a35b377a97e9a39d38de6f12cc26e1d7b4f0a39d
HG570 <rafaelpinheirovgp@gmail.com>
2024-04-25 20:21:00 -0300

    feat: QR Code button modal

--- src/components/layout/VacancyLine.js
+++ src/components/layout/VacancyLine.js
@@ -3,30 +3,21 @@ import { HiChevronDown, HiChevronUp, HiOutlinePlusSmall } from 'react-icons/hi2'
 import { useState } from 'react'
 import { useCollapse } from 'react-collapsed'
 import VacanciesNumber from './VacanciesNumber'
-
-function VacancyStation() {
-    return (
-        <>
-            <li className={styles.stationCard}>
-                <p>
-                Estação Teste 
-                </p>
-                <VacanciesNumber/>
-                <button><HiOutlinePlusSmall /></button>
-            </li>
-        </>
-    )
-}
+import QRCodeVacancy from './QRCodeVacancy'
 
 function VacancyLine() {
+    const [openModal, setOpenModal] = useState(false)
     const number = "0"
     const line = "Teste"
+    let color = "var(--red-cptm)"
+    document.documentElement.style.setProperty('--lineColor', color);
+
     const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
 
     return (
         <>
             <li className={styles.vacancyList}>
-                <section className={styles.card}>
+                <section className={`${styles.card} `}>
                     <h3>Linha {number} - {line}</h3>
                     <button {...getToggleProps()}>
                         {isExpanded ? <HiChevronUp /> : <HiChevronDown />}
@@ -34,12 +25,31 @@ function VacancyLine() {
                 </section>
                 <section {...getCollapseProps()}>
                     <ul>
-                        <VacancyStation />
-                        <VacancyStation />
-                        <VacancyStation />
+                        <li className={styles.stationCard}>
+                            <p>
+                                Estação Teste
+                            </p>
+                            <VacanciesNumber />
+                            <button onClick={() => setOpenModal(true)}><HiOutlinePlusSmall /></button>
+                        </li>
+                        <li className={styles.stationCard}>
+                            <p>
+                                Estação Teste
+                            </p>
+                            <VacanciesNumber />
+                            <button><HiOutlinePlusSmall /></button>
+                        </li>
+                        <li className={styles.stationCard}>
+                            <p>
+                                Estação Teste
+                            </p>
+                            <VacanciesNumber />
+                            <button><HiOutlinePlusSmall /></button>
+                        </li>
                     </ul>
                 </section>
             </li>
+            <QRCodeVacancy isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)} />
         </>
     )
 }
