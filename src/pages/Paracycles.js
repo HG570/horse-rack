de1717c0adcd3a3715a2eaa0e0c2a1249c16b900
HG570 <rafaelpinheirovgp@gmail.com>
2024-08-09 18:00:17 -0300

    build: paracycles page

---
+++ src/pages/Paracycles.js
@@ -0,0 +1,13 @@
+import TrainLine from '../components/layout/TrainLine';
+
+function Paracycles() {
+
+    return (
+        <>
+            <h1>Paraciclos</h1>
+            <p>Permitem estacionar a bicicleta próximo à estação. O espaço é aberto e não há monitoramento do local, portanto a utilização ocorre por conta da responsabilidade do próprio ciclista.</p>
+        </>
+    )
+}
+
+export default Paracycles;
