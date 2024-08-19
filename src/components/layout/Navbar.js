87984ad52beedfd35bbe60ceab0ed17e6345cb28
HG570 <rafaelpinheirovgp@gmail.com>
2024-08-19 00:32:50 -0300

    feat: add qr code button and remove link for notifications

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,33 +1,33 @@
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
-import { HiMiniHome, HiMiniBell, HiMiniUser } from 'react-icons/hi2'
+import { HiMiniHome, HiOutlineQrCode, HiMiniUser } from 'react-icons/hi2'
 
-function Navbar(){
+function Navbar({ onOpenQRCode, onClose }) {
     return (
-        <footer>
-            <div className={styles.background_navbar}>
-            <ul className={styles.navbar}>
-                <li>
-                    <Link to="/">
-                        <HiMiniHome />
-                        Início
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/notificacoes">
-                        <HiMiniBell />
-                        Notificações
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/conta">
-                        <HiMiniUser />
-                        Conta
-                    </Link>
-                </li>
-            </ul>
-        </div>
-        </footer>
+        <>
+            <section className={styles.background_navbar}>
+                <ul className={styles.navbar}>
+                    <li>
+                        <Link to="/" onClick={onClose}>
+                            <HiMiniHome />
+                            Início
+                        </Link>
+                    </li>
+                    <li>
+                        <button onClick={onOpenQRCode}>
+                            <HiOutlineQrCode />
+                            QRCode
+                        </button>
+                    </li>
+                    <li>
+                        <Link to="/conta" onClick={onClose}>
+                            <HiMiniUser />
+                            Conta
+                        </Link>
+                    </li>
+                </ul>
+            </section>
+        </>
     )
 }
 
