35b9a9f0d3f24377ecf27711156c7908d578da6a
HG570 <rafaelpinheirovgp@gmail.com>
2024-08-19 00:34:18 -0300

    fix: adjust open and close attributes

--- src/components/layout/QRCodeVacancy.js
+++ src/components/layout/QRCodeVacancy.js
@@ -1,9 +1,8 @@
 import styles from './QRCodeVacancy.module.css';
-import VacanciesNumber from './VacanciesNumber';
 import { HiOutlineXMark } from 'react-icons/hi2';
 import QRCodeGenerator from '../utils/QRCodeGenerator';
 
-export default function QRCodeVacancy({ isOpen, setQRCodeOpen, trainLine, name, numberOfVacancies, maxNumberOfVacancies }) {
+export default function QRCodeVacancy({ isOpen, onClose }) {
     let color = "var(--red-cptm)";
     document.documentElement.style.setProperty('--lineColor', color);
     const userName = "Rafael Pinheiro";
@@ -14,19 +13,14 @@ export default function QRCodeVacancy({ isOpen, setQRCodeOpen, trainLine, name, 
         return (
             <section className={styles.qrcodeCard}>
                 <article className={`${styles.card} `}>
-                    <h3>{trainLine}</h3>
-                    <button onClick={setQRCodeOpen}>
+                    <h3>QR Code</h3>
+                    <button onClick={onClose}>
                         <HiOutlineXMark />
                     </button>
-                </article>
-                <article className={styles.station}>
-                    <h4>{name}</h4>
-                    <VacanciesNumber number={numberOfVacancies} maxNumber={maxNumberOfVacancies} />
                 </article>
                 <article className={styles.data}>
                     <QRCodeGenerator value={userToken} />
                     <ul>
-                        <li>Nome: {userName}</li>
                         <li>Documento: {documento}</li>
                     </ul>
                 </article>
