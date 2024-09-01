e3f189c8239bf8ac803a696d2c525a167d537821
HG570 <rafaelpinheirovgp@gmail.com>
2024-09-01 19:55:28 -0300

    feat: remove link and add box shadow

--- src/components/layout/MainMenu.js
+++ src/components/layout/MainMenu.js
@@ -1,29 +1,24 @@
-import { Link } from 'react-router-dom'
 import styles from '../layout/MainMenu.module.css'
 import bikerack_banner from '../../img/bikerack_banner.jpg'
 import ciclista_cidadao from '../../img/ciclista_cidadao.png'
 import paracycles from '../../img/paracycles.jpg'
+import { useNavigate } from 'react-router-dom'
 
 function MainMenu() {
+    const navigate = useNavigate();
     return(
         <section className={styles.container}>
-            <article className={styles.card}>
-                <Link to="/bicicletarios">
+            <article className={styles.card} onClick={() => (navigate("/bicicletarios"))}>
                     <img src={bikerack_banner} alt="Foto Bicicletário"/>
                     <h3>Bicicletários</h3>
-                </Link>
             </article>
-            <article className={styles.card}>
-                <Link to="/ciclista-cidadao">
+            <article className={styles.card} onClick={() => {navigate("/ciclista-cidadao")}}>
                     <img src={ciclista_cidadao} alt="Logo Ciclista Cidadão"/>
                     <h3>Ciclista Cidadão</h3>  
-                </Link>
             </article>
-            <article className={styles.card}>
-                <Link to="/paraciclos">
+            <article className={styles.card} onClick={() => {navigate("/paraciclos")}}>
                     <img src={paracycles} alt="Foto Paraciclos"/>
                     <h3>Paraciclos</h3>
-                </Link>
             </article>
         </section>
     )
