0acffe92ab29829ba928c3a0014a0e1d97a18857
HG570 <rafaelpinheirovgp@gmail.com>
2024-09-01 19:20:54 -0300

    fix: change history back button function

--- src/components/button/Back.js
+++ src/components/button/Back.js
@@ -1,13 +1,13 @@
-import { useNavigate } from 'react-router-dom'
+//import { useNavigate } from 'react-router-dom'
 import { HiMiniArrowLeftCircle } from "react-icons/hi2"
 import styles from './Back.module.css'
 
 function Back() {
-    const history = useNavigate()
+    const goBack = () => {  window.history.back();}
 
     return (
         <>
-            <button onClick={() => history(-1)} className={styles.back}>
+            <button onClick={goBack} className={styles.back}>
                 <HiMiniArrowLeftCircle viewBox="2 2 16 16" />
             </button>
         </>
