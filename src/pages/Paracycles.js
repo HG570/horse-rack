40b60565167974aaf2130d8c199a1855f09ce7ca
HG570 <rafaelpinheirovgp@gmail.com>
2024-09-01 21:22:59 -0300

    feat: add paracycle data

--- src/pages/Paracycles.js
+++ src/pages/Paracycles.js
@@ -1,11 +1,14 @@
 import TrainLine from '../components/layout/TrainLine';
+import styles from './Paracycles.module.css'
 
 function Paracycles() {
 
     return (
         <>
-            <h1>Paraciclos</h1>
-            <p>Permitem estacionar a bicicleta próximo à estação. O espaço é aberto e não há monitoramento do local, portanto a utilização ocorre por conta da responsabilidade do próprio ciclista.</p>
+            <p className={styles.text}>Permitem estacionar a bicicleta próximo à estação. O espaço é aberto e não há monitoramento do local, portanto a utilização ocorre por conta da responsabilidade do próprio ciclista.</p>
+            <ul className={styles.container}>
+                <TrainLine name="Linha 12 - Safira" bikeRacks={[{"name": "São Miguel Paulista", "vacancies": "33", "trainLine": "Linha 12 - Safira"}]} lineColor="#1C146B"></TrainLine>
+            </ul>
         </>
     )
 }
