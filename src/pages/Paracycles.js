355659be4135995b5cc4602baf07c7b2c7e6eea6
HG570 <rafaelpinheirovgp@gmail.com>
2024-11-29 06:50:51 -0300

    fix: add number of vacancies

--- src/pages/Paracycles.js
+++ src/pages/Paracycles.js
@@ -1,16 +1,41 @@
 import TrainLine from '../components/layout/TrainLine';
 import styles from './Paracycles.module.css'
+import InfoRack from '../components/layout/InfoRack';
+import { useState } from 'react';
 
 function Paracycles() {
+    const [openRackInfo, setOpenRackInfo] = useState(false);
+    const [rackInfoData, setRackInfoData] = useState({ trainLine: '', name: '', numberOfVacancies: '', maxNumberOfVacancies: '', lineColor: '' });
+
+    const trainLineData = {
+        name: "Linha 12 - Safira",
+        lineColor: "#1C146B",
+        bikeRacks: [
+            { bikeRackId: 1, name: "São Miguel Paulista", availableVacancies: 33, maxNumberOfVacancies:33},
+        ]
+    };
+
+    const handleRackInfoOpen = (trainLine, name, numberOfVacancies, maxNumberOfVacancies, lineColor ) => {
+        setRackInfoData({ trainLine, name, numberOfVacancies, maxNumberOfVacancies, lineColor });
+        setOpenRackInfo(true);
+    }
 
     return (
         <>
+            <InfoRack
+                    infoOpen={openRackInfo} 
+                    handleRackInfoOpen={() => setOpenRackInfo(false)} 
+                    trainLine={rackInfoData.trainLine}
+                    lineColor={rackInfoData.lineColor}
+                    name={rackInfoData.name} 
+                    numberOfVacancies={rackInfoData.numberOfVacancies} 
+                    maxNumberOfVacancies={rackInfoData.numberOfVacancies} 
+            />
             <p className={styles.text}>Permitem estacionar a bicicleta próximo à estação. O espaço é aberto e não há monitoramento do local, portanto a utilização ocorre por conta da responsabilidade do próprio ciclista.</p>
             <ul className={styles.container}>
-                <TrainLine name="Linha 12 - Safira" bikeRacks={[{"name": "São Miguel Paulista", "vacancies": "33", "trainLine": "Linha 12 - Safira"}]} lineColor="#1C146B"></TrainLine>
+                <TrainLine name={trainLineData.name} bikeRacks={trainLineData.bikeRacks} lineColor={trainLineData.lineColor} handleRackInfoOpen={(handleRackInfoOpen)}></TrainLine>
             </ul>
         </>
     )
 }
-
 export default Paracycles;
