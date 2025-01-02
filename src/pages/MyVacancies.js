870117ad32dc6847bcf087c8c1de3dd9fcf28607
HG570 <rafaelpinheirovgp@gmail.com>
2025-01-02 20:12:49 -0300

    feat: add dynamic component rendering

--- src/pages/MyVacancies.js
+++ src/pages/MyVacancies.js
@@ -1,14 +1,46 @@
-import BikeCard from "../components/layout/BikeCard";
 import styles from "./MyVacancies.module.css"
+import { getMyVacancy } from "./../services/MyVacancy"
+import { useEffect, useState } from 'react';
+import BikeCard from "../components/layout/BikeCard";
 
 function MyVacancies() {
+    const [vacancy, setVacancy] = useState(null);
+    const userId = localStorage.getItem('userId');
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const userVacancy = await getMyVacancy(userId);
+
+                console.log(userVacancy);
+                setVacancy(userVacancy);
+            } catch (error) {
+                console.error('Error fetching profile:', error);
+            }
+        };
+
+        fetchProfile();
+    }, [userId]);
+
+    if (vacancy === null) {
+        return <p>Loading...</p>;
+    }
+
+    const isVacancyEmpty = !vacancy.bikeRack && !vacancy.entryDate && !vacancy.attendant;
+
     return (
         <>
             <div className={styles.cards}>
-                <BikeCard />
-                <BikeCard />
+                {isVacancyEmpty ? (
+                    <p>Não há bicicletários cadastrados.</p>
+                ) : (
+                    <BikeCard
+                        bikeRack={vacancy.bikeRack?.name}
+                        railwayLine={vacancy.bikeRack?.railwayLine}
+                        entryDate={vacancy.entryDate}
+                        attendant={vacancy.attendant}
+                    />
+                )}
             </div>
-            {/* Your component to display the list of vacancies */}
         </>
     );
 }
