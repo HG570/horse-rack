4f7d9ddb9484289fe34bed8a2c2abb42e4610efc
HG570 <rafaelpinheirovgp@gmail.com>
2025-01-02 19:43:14 -0300

    feat: add options for all brazilian states

--- src/components/forms/RegisterForm.js
+++ src/components/forms/RegisterForm.js
@@ -34,7 +34,7 @@ function RegisterForm() {
     };
 
     const [error, setError] = useState(null);
-    
+
     const [currentStep, setCurrentStep] = useState(1);
 
     const [loadingCep, setLoadingCep] = useState(false);
@@ -89,9 +89,9 @@ function RegisterForm() {
         try {
             await signUp(formData);
             alert('Cadastro realizado com sucesso!');
-            // Redirecionar ou limpar o formulário após o sucesso
         } catch (error) {
             setError('Falha ao realizar o cadastro. Tente novamente.');
+            alert('Falha ao realizar o cadastro. Tente novamente.');
             event.preventDefault();
         }
     };
@@ -103,10 +103,10 @@ function RegisterForm() {
             return;
         } else if (formData.name === null && formData.email === null && formData.password === null) {
             setError('Você deve inserir todos os dados.');
-        } else if (currentStep===2 && formData.docType === null && formData.document === null && formData.birthDate === '') {
+        } else if (currentStep === 2 && formData.docType === null && formData.document === null && formData.birthDate === '') {
             setError('Você deve inserir todos os dados.');
         } else {
-        setCurrentStep(prevStep => prevStep + 1);
+            setCurrentStep(prevStep => prevStep + 1);
         }
     };
 
@@ -137,13 +137,12 @@ function RegisterForm() {
             {currentStep === 2 && (
                 <section>
                     <article>
-                        <select list="documents" id="documentType" name="docType" value={Object.keys(documentTypeMapping).find(key => documentTypeMapping[key] === formData.docType) || ''}  onChange={handleChange} required >
-
-                                <option value="RG">RG</option>
-                                <option value="CPF">CPF</option>
-                                <option value="CNH">CNH</option>
-                                <option value="PASSAPORTE">PASSAPORTE</option>
-                                <option value="CRNM">CRNM</option>
+                        <select list="documents" id="documentType" name="docType" value={Object.keys(documentTypeMapping).find(key => documentTypeMapping[key] === formData.docType) || ''} onChange={handleChange} required >
+                            <option value="RG">RG</option>
+                            <option value="CPF">CPF</option>
+                            <option value="CNH">CNH</option>
+                            <option value="PASSAPORTE">PASSAPORTE</option>
+                            <option value="CRNM">CRNM</option>
                         </select>
                         <label htmlFor="documentType">Tipo de Documento</label>
                     </article>
@@ -182,12 +181,41 @@ function RegisterForm() {
                         <label htmlFor="city">Cidade</label>
                     </article>
                     <article>
-                        <input type="text" id="state" name="addressDTO.state" value={formData.addressDTO.state} onChange={handleChange} required />
+                        <select list="states" id="state" name="addressDTO.state" value={formData.addressDTO.state} onChange={handleChange} required>
+                            <option value="" disabled>Selecione um estado</option>
+                            <option value="AC">Acre</option>
+                            <option value="AL">Alagoas</option>
+                            <option value="AP">Amapá</option>
+                            <option value="AM">Amazonas</option>
+                            <option value="BA">Bahia</option>
+                            <option value="CE">Ceará</option>
+                            <option value="DF">Distrito Federal</option>
+                            <option value="ES">Espírito Santo</option>
+                            <option value="GO">Goiás</option>
+                            <option value="MA">Maranhão</option>
+                            <option value="MT">Mato Grosso</option>
+                            <option value="MS">Mato Grosso do Sul</option>
+                            <option value="MG">Minas Gerais</option>
+                            <option value="PA">Pará</option>
+                            <option value="PB">Paraíba</option>
+                            <option value="PR">Paraná</option>
+                            <option value="PE">Pernambuco</option>
+                            <option value="PI">Piauí</option>
+                            <option value="RJ">Rio de Janeiro</option>
+                            <option value="RN">Rio Grande do Norte</option>
+                            <option value="RS">Rio Grande do Sul</option>
+                            <option value="RO">Rondônia</option>
+                            <option value="RR">Roraima</option>
+                            <option value="SC">Santa Catarina</option>
+                            <option value="SP">São Paulo</option>
+                            <option value="SE">Sergipe</option>
+                            <option value="TO">Tocantins</option>
+                        </select>
                         <label htmlFor="state">Estado</label>
                     </article>
                     <p>Ao criar uma conta, você concorda com nossos Termos, Política de Privacidade e Política de Cookies.</p>
-                    
-                    <button onClick={() => {handleSubmit(); navigate('/signin');}}>Criar Conta</button>
+
+                    <button onClick={() => { handleSubmit(); navigate('/signin'); }}>Criar Conta</button>
                 </section>
             )}
         </form>
